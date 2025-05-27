177a2f22a8a68ec99a26bfb1c0f095b010cbf3e4
hanssonbjorn <35796262+hanssonbjorn@users.noreply.github.com>
2025-05-27 11:23:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 function cpuWork() {
   let sum = 0;
   for (let i = 0; i < 1e9; i++) {
-    sum += Math.sqrt(i)
+    sum += Math.sqrt(i);
   }
   return sum;
 }
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
   const result = cpuWork();
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end(
+  res.end(`
     <h1>CPU-belastningstest</h1>
     <p>Appen kördes korrekt och genererade CPU-arbete.</p>
     <p>Resultat av beräkning: ${result}</p>
